31b240ca301b8fd33e0ea3996e385ad6603ec16d
Youssef Serag <yserag@asu.edu>
2023-11-27 14:15:45 -0700

    Completed second lab

--- samosa-selector/src/App.jsx
+++ samosa-selector/src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import samosa from './samosa.jpg'
+import React from 'react';
+import {useState} from 'react';
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+  
+  // Function that updates the count variable.
+  const updateCount = () => setCount(count + multiplier);
+
+  // Function that updates the multiplier variable.
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setCount(count - 10);
+      setMultiplier(multiplier * 2);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setCount(count - 100);
+      setMultiplier(multiplier * 5);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setCount(count - 1000);
+      setMultiplier(multiplier * 10);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h1>Samosa Selector</h1>
+      <h2>Count: {count}</h2>
+      <img className='samosa' src={samosa} onClick={updateCount} />
+      <div className='container'>
+        <div className='upgrade'>
+          <h3>
+            Double Stuffed
+          </h3>
+          <p>
+            2x per click
+          </p>
+          <button className='button' onClick={buyDoubleStuffed}>
+            Cost: 10 samosas
+          </button>
+        </div>
+        <div className='upgrade'>
+          <h3>
+            Party Pack
+          </h3>
+          <p>
+            5x per click
+          </p>
+          <button className='button' onClick={buyFullFeast}>
+            Cost: 100 samosas
+          </button>
+        </div>
+        <div className='upgrade'>
+          <h3>
+            Full Feast
+          </h3>
+          <p>
+            10x per click
+          </p>
+          <button className='button' onClick={buyFullFeast}>
+            Cost: 1000 samosas
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {/* Creating the multiplier buttons */}
+    </div>
   )
 }
 
