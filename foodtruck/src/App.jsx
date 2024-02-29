b05c914147d098aa2fe31f2ccbb624ff6f89fb52
Youssef Serag <yserag@asu.edu>
2024-02-29 16:52:46 -0700

    Completed Lab 1 per specfications

--- foodtruck/src/App.jsx
+++ foodtruck/src/App.jsx
@@ -1,20 +1,85 @@
-import './App.css'
-import Card from './components/Card'
-import mexican from './images/mexican.jpg'
+import "./App.css";
+import Card from "./components/Card";
+import mexican from "./images/mexican.jpg";
+import poke from "./images/poke.png";
+import olive from "./images/italian.jpg";
+import chicken from "./images/fried_chicken.jpg";
+import middle_eastern from "./images/middle_eastern.jpg";
+import brisket from "./images/brisket.jpg";
+import breakfast from "./images/breakfast.jpg";
+import pho from "./images/pho.jpg";
+import acai from "./images/acai.jpg";
+import cookies from "./images/cookies.jpg";
+import banner from "./images/awning.png";
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='title'>Food Truck Finder</h1>
-      <div className='container'>
-        <Card path={mexican} name='Los Pollos Hermanos' cuisine='Mexican' link='https://www.chipotle.com/'/>
-        <Card name="Poke2U"/>
-        <Card name="Olive Garden"/>
-        <Card name="Dave's Hot Chicken"/>
-        <Card name='Green Corner'/>
+    <div className="App">
+      <h1 className="title">Favorite Restaurants</h1>
+      <div className="container">
+        <Card
+          path={mexican}
+          name="Los Pollos Hermanos"
+          cuisine="Mexican"
+          link="https://www.chipotle.com/"
+        />
+        <Card
+          path={poke}
+          name="Poke2U"
+          cuisine="Hawaiin"
+          link="https://www.poke2u.com/"
+        />
+        <Card
+          path={olive}
+          name="Olive Garden"
+          cuisine="Italian"
+          link="https://www.olivegarden.com/home"
+        />
+        <Card
+          path={chicken}
+          name="Dave's Hot Chicken"
+          cuisine="Nashville Fried Chicken"
+          link="https://www.daveshotchicken.com/"
+        />
+        <Card
+          path={middle_eastern}
+          name="Green Corner"
+          cuisine="Middle Eastern"
+          link="https://greencorneraz.com/"
+        />
+        <Card
+          path={brisket}
+          name="Texas Brisket"
+          cuisine="Texas BBQ"
+          link="https://littlemissbbq.com/"
+        />
+        <Card
+          path={breakfast}
+          name="First Watch Breakfast"
+          cuisine="American Breakfast"
+          link="https://www.firstwatch.com/"
+        />
+        <Card
+          path={pho}
+          name="Pho Valley"
+          cuisine="Viatnemese"
+          link="https://phofreshalleyandbobatea.com/"
+        />
+        <Card
+          path={acai}
+          name="Berry Divine Acai"
+          cuisine="Tropical"
+          link="https://berrydivineacai.com/"
+        />
+        <Card
+          path={cookies}
+          name="Crumbl Cookies"
+          cuisine="Dessert"
+          link="https://crumblcookies.com/"
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- foodtruck/src/components/Card.jsx
+++ foodtruck/src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import '../App.css';
+import "../App.css";
 
 const Card = (props) => {
-
-    return (
-        <div className="card">
-            <img src={props.path} className="card-img"></img>
-            <h3 className="card-h4">{props.name}</h3>
-            <h4 className="card-h4">{props.cuisine}</h4>
-            <a href={props.link}>
-                <button>
-                    View Menu
-                </button>
-            </a>
-        </div>
-    )
-}
+  return (
+    <div className="card">
+      <img src={props.path} className="card-img"></img>
+      <div className="text-holder">
+        <h3 className="card-h3">{props.name}</h3>
+        <h4 className="card-h4">{props.cuisine}</h4>
+      </div>
+      <a href={props.link}>
+        <button className="blue-button">View Menu</button>
+      </a>
+    </div>
+  );
+};
 
 export default Card;
