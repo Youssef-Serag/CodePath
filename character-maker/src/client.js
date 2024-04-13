d7c360c6cbdb364d9cd8b4d1e9dcd0f722b7748c
Youssef Serag <yserag@asu.edu>
2024-04-12 19:14:12 -0700

    Deleted folder and will be starting over

--- character-maker/src/client.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const URL = "https://cjthtbhkdeemfldymjjj.supabase.co";
-const API_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdGh0YmhrZGVlbWZsZHltampqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI5NTk1MTYsImV4cCI6MjAyODUzNTUxNn0.cV0IzrTl6qRSV_n_U35jdxy3WVBCD_rM3OaTEwMfkWk";
-export const supabase = createClient(URL, API_KEY);
